4f1532449151eaac2546b355283850dc5704bbc6
zasghar26 <151073341+zasghar26@users.noreply.github.com>
2025-09-17 19:58:21 +0500

    Update route.js

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,15 +1,12 @@
-// app/api/inngest/route.js
 export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
-// (keep the rest)
 import { serve } from "inngest/next";
 import { inngest } from "../../../src/inngest/client.js";
-import { welcomeEmail } from "../../../src/inngest/functions/welcomeEmail.js";
-import { nightlyReport } from "../../../src/inngest/functions/nightlyReport.js";
+import { userCreated } from "../../../src/inngest/functions/userCreated.js";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [welcomeEmail, nightlyReport],
+  functions: [userCreated],
 });
