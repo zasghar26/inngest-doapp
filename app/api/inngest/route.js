ca9565d5fac0419af1d4eba66e081f8542701750
zasghar26 <151073341+zasghar26@users.noreply.github.com>
2025-09-17 20:45:24 +0500

    Update route.js

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -6,6 +6,13 @@ import { serve } from "inngest/next";
 import { inngest } from "../../../src/inngest/client.js";
 import { userCreated } from "../../../src/inngest/functions/userCreated.js";
 
+// Log presence (not values) once per cold start
+console.log("[/api/inngest] env presence:", {
+  appId: !!process.env.INNGEST_APP_ID,
+  eventKey: !!process.env.INNGEST_EVENT_KEY,
+  signingKey: !!process.env.INNGEST_SIGNING_KEY,
+});
+
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [userCreated],
